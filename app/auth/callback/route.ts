9e43861dffad7cd6ea4885707d4828012954a932
mori10tech <web.mori10@gmail.com>
2026-08-26 13:52:20 +0900

    fix: LINEトーク遷移とLIFF診断導線を最適化

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -10,7 +10,7 @@ type LineFriendshipResponse = {
 };
 
 const AKANUKE_LINE_TALK_URL =
-  "https://line.me/R/oaMessage/@507rwrwg";
+  "https://line.me/R/oaMessage/%40507rwrwg";
 
 function createLoginRedirect(
   request: NextRequest,

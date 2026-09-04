afd4ee4fe64d9ff99552f9628ec023485ba0ac7b
mori10tech <web.mori10@gmail.com>
2026-09-04 14:56:11 +0900

    セキュリティ: 基本セキュリティヘッダを追加

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,29 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "Referrer-Policy",
+            value:
+              "strict-origin-when-cross-origin",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
